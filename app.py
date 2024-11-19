424a35ffc34bf528017f448d251b20f6f580eb4f
xchbx <ceina.ellison@gmail.com>
2024-11-19 14:11:05 -0500

    set up quiz

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template('home.html')
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.config['ENV'] = 'development'
+    app.run(debug=True)
